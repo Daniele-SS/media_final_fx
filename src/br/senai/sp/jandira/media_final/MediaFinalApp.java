b85c8f3bf5d4afd368f17758dec610b2fd6dd391
DANIELE SILVA SANTOS <25203622@CFP127ALUNO.LOCAL>
2025-10-20 11:25:52 -0300

    Start do projeto

---
+++ src/br/senai/sp/jandira/media_final/MediaFinalApp.java
@@ -0,0 +1,102 @@
+package br.senai.sp.jandira.media_final;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import javax.swing.border.Border;
+
+public class MediaFinalApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        //Determinar o tamanho do stage (tela/janela)
+        stage.setWidth(600);
+        stage.setHeight(500);
+
+        //Determinar o título do stage (tela/janela)
+        stage.setTitle("Média Final");
+
+        //Painel Raiz (root)
+        BorderPane root = new BorderPane();
+
+        Label labelTitulo = new Label();
+        labelTitulo.setText("Escola \"Prof. Vicente Amato\"");
+        //Formatação do texto da Label
+        labelTitulo.setStyle("-fx-text-fill: #005aff; -fx-font-size: 32; -fx-font-weight: bold;");
+        labelTitulo.setPadding(new Insets(10, 0, 10, 10));
+
+        root.setTop(labelTitulo);
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+
+        VBox painelResultado = new VBox();
+        painelResultado.setPadding(new Insets(10, 0, 10, 10));
+        Label labelAluno = new Label("Nome do aluno: ");
+        Label labelMediaFinal = new Label("Média Final: ");
+        Label labelSituacao = new Label("Situação: ");
+        painelResultado.getChildren().addAll(labelAluno, labelMediaFinal, labelSituacao);
+
+        //Painel de botões
+        VBox painelDeBotoes = new VBox();
+        painelDeBotoes.setPadding(new Insets(10, 10, 10, 0));
+        painelDeBotoes.setSpacing(10);
+        Button buttonCalcularMedia = new Button("Cácular média");
+        buttonCalcularMedia.setPrefWidth(100);
+        buttonCalcularMedia.setPrefHeight(-100);
+
+        Button buttonLimpar = new Button("Limpar");
+        buttonLimpar.setPrefWidth(100);
+        buttonLimpar.setPrefHeight(-100);
+
+        Button buttonSair = new Button("Sair");
+        buttonSair.setPrefWidth(100);
+        buttonSair.setPrefHeight(-100);
+        painelDeBotoes.getChildren().addAll(buttonCalcularMedia, buttonLimpar, buttonSair);
+
+        //Painel de formulário
+        VBox paineFormulario = new VBox();
+        paineFormulario.setPadding(new Insets(10, 0, 10, 10));
+        Label labelNome = new Label("Nome do Aluno:");
+        Label labelNota1 = new Label("Nota 1:");
+        Label labelNota2 = new Label("Nota 2:");
+        Label labelNota3 = new Label("Nota 3:");
+        Label labelNota4 = new Label("Nota 4:");
+
+        TextField textFieldNome = new TextField();
+        TextField textFieldNota1 = new TextField();
+        TextField textFieldNota2 = new TextField();
+        TextField textFieldNota3 = new TextField();
+        TextField textFieldNota4 = new TextField();
+
+        paineFormulario.getChildren().addAll(
+                labelNome, textFieldNome,
+                labelNota1, textFieldNota1,
+                labelNota2, textFieldNota2,
+                labelNota3, textFieldNota3,
+                labelNota4, textFieldNota4
+        );
+
+        root.setTop(labelTitulo);
+        root.setBottom(painelResultado);
+        root.setRight(painelDeBotoes);
+        root.setLeft(paineFormulario);
+
+        //Mostrar o stage (tela/janela)
+        stage.show();
+
+        //Eventos de clique dos botões
+        buttonCalcularMedia.addEventHandler();
+    }
+}
